a49c1502cb6960e657e2d4f9d108b8af7a10035a
wenhe-liu <liu.wen.he@chrjdt.com>
2017-03-06 15:40:15 +0800

    0.1.0

--- Example/PlayGifImage/ViewController.swift
+++ Example/PlayGifImage/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import PlayGifImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lastImageView: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //加载普通图片,但是放在images.xcassets里面的图片是加载不出来的
+        LoadGifImageTool.loadGifImage(imageView, "ting.jpeg")
+        //加载无限播放的gif图片
+        LoadGifImageTool.loadGifImage(lastImageView, "gif.jpg", true)
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Example/Pods/Target Support Files/PlayGifImage/PlayGifImage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PlayGifImage : NSObject
+@end
+@implementation PodsDummy_PlayGifImage
+@end
